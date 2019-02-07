25ac4fbe838d7a23a03ef6aef6353a8c5d08d842
Marcos Bracco <mb@o-log-n.com>
2019-02-07 19:33:14 -0300

    path fixes

--- test.bat
+++ test.bat
@@ -2,4 +2,4 @@
 set PATH=%cd%\llvm\utils\lit;%PATH%
 set PATH=%cd%\build\release\bin;%PATH%
 
-llvm-lit.py llvm\tools\clang\tools\nodecpp-checker\test\nodecpp-checker
+llvm-lit.py llvm\tools\clang\tools\nodecpp\test\nodecpp-checker
